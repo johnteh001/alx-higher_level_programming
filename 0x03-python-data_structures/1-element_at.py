28200a3273960703199cd072586904347bbc788b
John Karson <casonj254@gmail.com>
2023-12-03 21:52:46 -0500

    python

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Args:
+        my_list (list): The input list.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        int or None: The retrieved element, or None if index is out of range.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
