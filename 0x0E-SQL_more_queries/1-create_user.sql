76724610683590227e6d866e5617caa83a854488
john karson <casonj254@gmail.com>
2024-03-13 11:51:52 -0700

    sql10-16

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create MySQL server user
+-- user have all previleges on the server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
