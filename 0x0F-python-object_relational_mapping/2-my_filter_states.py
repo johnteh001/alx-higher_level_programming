5f78458cca9b016a20d6def8a143c103bff53a3b
John Karson <casonj254@gmail.com>
2024-04-11 23:28:12 +0300

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script list all States with name starting with N"""
+
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    pass_word = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+    db = MySQLdb.connect(user=user_name, passwd=pass_word, db=database,
+                         host="localhost", port=3306)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE BINARY states.name='{}'
+            ORDER BY states.id ASC""".format(state))
+    rows = c.fetchall()
+    for r in rows:
+        print(r)
+
+    c.close()
+    db.close()
